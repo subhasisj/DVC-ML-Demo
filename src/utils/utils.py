7080d35e405f3da630f82614a914e205113876f6
subhasisj <monty.rulz@gmail.com>
2021-10-10 01:55:07 +0530

    Added Config and Create file to load configs from YAML

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,3 +1,4 @@
+from typing import ForwardRef
 import yaml
 import os
 import sys
@@ -8,6 +9,12 @@ def read_yaml(path_to_yaml: str) -> dict:
     with open(path_to_yaml) as yaml_file:
         content = yaml.load(yaml_file,Loader=yaml.FullLoader)
     return content
+
+
+def create_directory(dir_paths: list):
+    for dir_path in dir_paths:
+        os.makedirs(dir_path,exist_ok=True)
+        print(f"creating director:{dir_path}")
 
 
 if __name__ == "__main__":
